b4347214ab1fcc31d9ae2e353277084066c1e035
cc26 <reynold8002@gmail.com>
2014-05-04 12:04:19 -0400

    skeleton

--- stencil/code/part1/create_index.py
+++ stencil/code/part1/create_index.py
@@ -1,10 +1,28 @@
+import argparse
+import json 
+
+# retrieve a list of postings containing a 
+# business id, review id, and position of each term occurrence. 
+# instead of using the review id, 
+# use the line on which the review occurs in the yelp dataset file as a unique identifier
+
+def create_index(data):
 
 
+	for line in data:
+		line_json = json.loads(line)
+
+def create_entry(line):
+	entry = {}
 
 def main():
-
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-path', required=True, help='Path to data')
+	opts = parser.parse_args()
 	
+	data = open(opts.path)
+	create_index(data)
 
-
+	data.close()
 if __name__ == '__main__':
 	main()
---
+++ stencil/code/part1/run.sh
@@ -0,0 +1,1 @@
+python create_index.py -path /course/cs1951a/pub/final/data/extracted/yelp_academic_dataset_review.json
